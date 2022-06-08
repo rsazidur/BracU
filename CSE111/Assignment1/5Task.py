8274d80cb3b38c09a66f22d3704dba9de7afa6f8
Molla Sazidur Rahman <rsazidur936@gmail.com>
2022-06-09 00:16:15 +0600

    Added task5

---
+++ CSE111/Assignment1/5Task.py
@@ -0,0 +1,37 @@
+def pass_check(password):
+    lower_count = sum(map(str.islower, password))
+    upper_count = sum(map(str.isupper, password))
+    digit_count = sum(map(str.isdigit, password))
+
+    spec_count = 0
+    special_char = "_@$#"
+    for spec in password:
+        if special_char in password:
+            spec_count += 1
+
+    error = ""
+    if lower_count == 0:
+        error += "Lowercase character missing"
+
+    if upper_count == 0:
+        if error != "":
+            error += ", "
+        error += "Uppercase character missing"
+
+    if digit_count == 0:
+        if error != "":
+            error += ", "
+        error += "Digit character is missing"
+
+    if spec_count == 0:
+        if error != "":
+            error += ", "
+        error += "Special character is missing"
+
+    if error == "":
+        print("OK")
+    else:
+        print(error)
+
+user = input("Enter the password: ")
+pass_check(user)
