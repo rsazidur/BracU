4782c4c536c58495b30a82a6f5b779481871c665
Molla Sazidur Rahman <rsazidur936@gmail.com>
2022-06-23 21:42:32 +0600

    Blue print

---
+++ CSE111/Assignment3/Task_4.py
@@ -0,0 +1,18 @@
+class Vehicle:
+    def __init__(self, up, down, left, right):
+        self.up = up
+        self.down = down
+        self.left = left
+        self.right = right
+
+    def moveUp(self):
+        pass
+    def moveDown(self):
+        pass
+    def moveLeft(self):
+        pass
+    def moveRight(self):
+        pass
+
+    def print_position(self):
+        pass
