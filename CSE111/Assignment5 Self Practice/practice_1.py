3602fbc8d6151c2ccad09ae760ce2b1e19339197
Molla Sazidur Rahman <rsazidur936@gmail.com>
2022-08-11 04:14:09 +0600

    practice_1 updated

--- CSE111/Assignment5 Self Practice/practice_1.py
+++ CSE111/Assignment5 Self Practice/practice_1.py
@@ -1,3 +1,23 @@
 class ABC:
     def __init__(self, x, y):
+        self.__x = x
+        self.y = y
+        
+    def details(self):
+        print(f"X is: {self.__x}\nY is: {self.y}")
+    
+    def set_X(self, x):
+        self.__x = x
+        
+    def get_X(self):
+        return self.__x
+    
+    
+a1 = ABC(5, 6)
+a2 = ABC(15, 17)
+
+a1.set_X(45)
+print(a1.get_X())
+a1.details()
+        
         
