d3f67a15fc357d6bc67ec79b0d8a5760fd002b24
Molla Sazidur Rahman <rsazidur936@gmail.com>
2022-08-11 20:49:47 +0600

    Static Variable Example

---
+++ CSE111/Assignment5 Self Practice/Static_variables_E.py
@@ -0,0 +1,21 @@
+class Student:
+    count = 0
+    
+    def __init__(self, name, Id):
+        self.name = name
+        self.id = Id
+        Student.count += 1
+        
+    def details(self):
+        print(f"Name: {self.name} Id: {self.id}")
+        
+
+s1 = Student("Bob", 11)
+s2 = Student("Carol", 22)
+s3 = Student("Mike", 33)
+
+s1.details()
+s2.details()
+s3.details()
+
+print(f"Student count: {Student.count}")
