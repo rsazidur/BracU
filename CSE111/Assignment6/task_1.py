56ea39d029153e151e0f9d3ecaa256f37ccb0bb7
Molla Sazidur Rahman <rsazidur936@gmail.com>
2022-08-12 04:01:12 +0600

    Task 1 added

--- CSE111/Assignment6/task_1.py
+++ CSE111/Assignment6/task_1.py
@@ -1,7 +1,33 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Aug 12 02:42:55 2022
+class Student:
+    ID = 0
+    
+    def __init__(self, name, department, age, cgpa):
+        self.name = name
+        self.department = department
+        self.age = age
+        self.cgpa = cgpa
+        Student.ID += 1
+        
+    def get_details(self):
+        print("ID:", Student.ID, "\nName:", self.name,
+              "\nDepartment:", self.department,
+              "\nAge:", self.age, "\nCGPA:", self.cgpa)
+        
+    
+    @classmethod
+    def from_String(cls, string):
+        name, department, age, cgpa = string.split("-")
+        obj = Student(name, department, age, cgpa)
+        return obj
 
-@author: rsazi
-"""
-
+s1 = Student("Samin", "CSE", 21, 3.91)
+s1.get_details()
+print("-----------------------")
+s2 = Student("Fahim", "ECE", 21, 3.85)
+s2.get_details()
+print("-----------------------")
+s3 = Student("Tahura", "EEE", 22, 3.01)
+s3.get_details()
+print("-----------------------")
+s4 = Student.from_String("Sumaiya-BBA-23-3.96")
+s4.get_details()
