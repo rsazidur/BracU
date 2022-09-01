af2e9501307162ea23246df57b1a5e6af4aa32b6
Molla Sazidur Rahman <rsazidur936@gmail.com>
2022-09-01 13:27:28 +0600

    Added

---
+++ CSE111/Assignment8/Final Exam Practice/problem_65.py
@@ -0,0 +1,29 @@
+class Employee:
+    def __init__(self, name, workingPeriod):
+        self.name = name
+        self.workingPeriod = workingPeriod
+    
+    @classmethod
+    def employeeByJoiningYear(cls, name, year):
+        workingPeriod = (2022 - year)
+        return Employee(name, workingPeriod)
+    
+    @staticmethod
+    def experienceCheck(workingPeriod, gender):
+        if workingPeriod < 3 and gender == "male":
+            return "He is not expreienced"
+        elif workingPeriod >= 3 and gender == "male":
+            return "He is exprienced"
+        elif workingPeriod < 3 and gender == "female":
+            return "She is not expreienced"
+        elif workingPeriod >= 3 and gender == "female":
+            return "She is exprienced"
+
+employee1 = Employee('Dororo', 3)
+employee2 = Employee.employeeByJoiningYear('Harry', 2016)
+print(employee1.workingPeriod)
+print(employee2.workingPeriod)
+print(employee1.name)
+print(employee2.name)
+print(Employee.experienceCheck(2, "male"))
+print(Employee.experienceCheck(3, "female"))
